2f6fa44dca9ed8bc4be5ef218de1700511912006
InpossibleMomo <momo.ylinen@outlook.com>
2021-04-29 16:44:38 +0300

    commit 2

---
+++ app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRoute = require('./controllers/blogRoute')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+      logger.info('connected to MongoDB')
+  })
+
+  app.use(cors())
+  app.use(express)
+  app.use(express.json())
+  app.use(middleware.requestLogger)
+  
+  app.use('/api/blogs', blogRoute)
+  
+  app.use(middleware.unknownEndpoint)
+  app.use(middleware.errorHandler)
+  
+  module.exports = app
